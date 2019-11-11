633d2d8b4a4b3759dd5090851b70e53ee52dbcd1
Mtorrents <martin.torrents35@gmail.com>
2019-11-10 23:24:31 -0300

    actualizacion

--- url/welcome.php
+++ url/welcome.php
@@ -10,6 +10,11 @@ include'session.php';
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/timepicker/1.3.5/jquery.timepicker.min.css">
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+  <link rel="stylesheet" href="/resources/demos/style.css">
+  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 <body>
 
 <nav class="navbar navbar-inverse">
@@ -35,32 +40,37 @@ include'session.php';
  <div class="form-group">
  	<label for="exampleFormControlSelect1">Especialidad</label>
  	<select class="form-control" id="exampleFormControlSelect1">
-	 <option value="1">Dermatologia</option>
-	 <option value="2">Pediatria</option>
+	 <option value="1">Clinica Medica</option>
+	 <option value="2">Dermatologia</option>
 	 <option value="3">Cardiologia</option>
-	 <option value="4">Clinica Medica</option>
 	</select>
 
 	<label for="exampleFormControlSelect1">Doctores</label>
 	<select class="form-control" id="exampleFormControlSelect1">	 
-	 <option value="1">Walter Gazzanego</option>
-	 <option value="2">Martin Torrents</option>
-	 <option value="3">Jose Viñuelas</option>
+	 <option value="1">Dr. Nick Riviera</option>
+	 <option value="2">Dr. Zoidberg</option>
+	 <option value="3">Dr. Hibbert</option>
 	</select>
-	<div class="input-append date form_datetime">
-    <input size="16" type="text" value="" readonly>
-    <span class="add-on"><i class="icon-th"></i></span>
-</div>
- 
-<script type="text/javascript">
-    $(".form_datetime").datetimepicker({
-        format: "dd MM yyyy - hh:ii"
-    });
-</script>
+  <script>
+  $( function() {
+    $( "#datepicker" ).datepicker();
+  } );
+  </script>
+<label for="exampleFormControlSelect1">Fecha</label>
+<br />
+<input type="text" id="datepicker">
+<br />
+<label for="exampleFormControlSelect1">Hora</label>
+<br />
+<input type="text" class="timepicker">
+<br />
+<br />
+<input type="submit">
 </div>   
 </form>	
 </div>	
 </body>
+</head>
 </html>
 
 
