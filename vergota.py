7d3cbd86768c9d51309ba51d6162d36341e724d6
walle <wgazzanego@gmail.com>
2022-05-02 14:11:12 -0300

    Create vergota.py

---
+++ vergota.py
@@ -0,0 +1,15 @@
+#leer el archivo de entrada
+entradaArchivo = open("f1.txt", "rt")
+#leer el contenido del archivo en una cadena
+texto = entradaArchivo.read()
+#reemplazar todas las ocurrencias de la cadena requerida
+texto = texto.replace('gode', 'vergota')
+#cerrar el archivo de entrada
+entradaArchivo.close()
+ 
+#abrir el archivo de entrada en modo de escritura
+entradaArchivo = open("f1.txt", "wt")
+#sobreescribir el archivo de entrada con los datos resultantes
+entradaArchivo.write(texto)
+#cerrar el archivo
+entradaArchivo.close()
